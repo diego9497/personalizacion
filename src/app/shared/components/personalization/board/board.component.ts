1b4e8f0c17a63f76cf1924c19108ef7fd034ad0d
Diego Aguilera <diego.aguilera@unillanos.edu.co>
2020-02-07 17:59:52 -0500

    Añadir canvas

--- src/app/shared/components/personalization/board/board.component.ts
+++ src/app/shared/components/personalization/board/board.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import "fabric";
+
+declare const fabric;
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.css"]
 })
 export class BoardComponent implements OnInit {
+  private canvas: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.canvas = new fabric.Canvas("canvas");
+    const circulo = new fabric.Cirlce({
+      radius: 20,
+      fill: "green",
+      left: 10,
+      top: 50
+    });
+    this.canvas.add(circulo);
+    this.canvas.setWidth(580);
+    this.canvas.setHeight(600);
   }
-
 }
