a7b2abd907e3b9dbdb341e9e3c108457502a7097
Diego Aguilera <diego.aguilera@unillanos.edu.co>
2020-02-10 12:02:18 -0500

    Imagen de fondo

--- src/app/shared/components/personalization/board/board.component.ts
+++ src/app/shared/components/personalization/board/board.component.ts
@@ -1,28 +1,51 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
-import "fabric";
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
 
-declare const fabric;
+import { fabric } from "fabric";
 
 @Component({
   selector: "app-board",
   templateUrl: "./board.component.html",
   styleUrls: ["./board.component.css"]
 })
-export class BoardComponent implements OnInit {
-  private canvas: any;
+export class BoardComponent implements OnInit, AfterViewInit {
+  private canvas: fabric.Canvas;
+  // private canvas: any;
+
+  @ViewChild("container", { static: true }) canvasContainer: ElementRef;
 
   constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
     this.canvas = new fabric.Canvas("canvas");
-    const circulo = new fabric.Cirlce({
-      radius: 20,
-      fill: "green",
-      left: 10,
-      top: 50
+    let height = this.canvasContainer.nativeElement.clientHeight;
+    let width = this.canvasContainer.nativeElement.clientWidth;
+    this.canvas.setHeight(height);
+    this.canvas.setWidth(width);
+    //let url = "https://cdn.shopify.com/s/files/1/2232/3715/products/8436567654301_CAMISETA_VAMOS_MIGUELITO_T19CAVAMIM__003_1920x.png?v=1548864278";
+    let url = "/assets/background/camiseta-blanca2.png";
+    // let img: fabric.Image;
+    fabric.Image.fromURL(url, img => {
+      console.log(img);
+      this.canvas.backgroundImage = img;
+
+      this.canvas.backgroundImage.scaleToWidth(width);
+      this.canvas.backgroundImage.scaleToHeight(height);
+      console.log(this.canvas.backgroundImage.width);
+      console.log(this.canvas.backgroundImage.height);
+      this.canvas.renderAll();
     });
-    this.canvas.add(circulo);
-    this.canvas.setWidth(580);
-    this.canvas.setHeight(600);
+
+    let circle = new fabric.Circle({
+      radius: 50
+    });
+    this.canvas.add(circle);
   }
 }
